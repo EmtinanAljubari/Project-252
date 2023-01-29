957366f46463bdff9f32055ed7b12bb46f47d3c8
EmtinanAljubari <ealjubari@gmail.com>
2023-01-29 22:32:14 +0300

    Refactoring switch cases2

--- src/gym/login/system/Gym_Login_System.java
+++ src/gym/login/system/Gym_Login_System.java
@@ -21,38 +21,43 @@ public class Gym_Login_System {
 
         System.out.print("Pleas enter your MemberID: ");
         int MemberID = input.nextInt();
+
+        int choice;
         System.out.println();
+        do {
+            System.out.println("----------------------------------------------------------------------------------------------");
+            System.out.println("              Hello this program is for -GYM LOGIN SYSTEM-                                    ");
+            System.out.println("----------------------------------------------------------------------------------------------");
+            System.out.println("               please enter the number of choices that you want *-*                           \n");
+            System.out.println(" 1: Reserve an appointment ");
+            System.out.println(" 2: Scan the chip of the Member ");
+            System.out.println(" 3: Scan the chip of the Employee ");
+            System.out.println(" 4: Print report Management");
+            System.out.println(" 5: End program");
+            System.out.print("Pleas enter your choice Here:");
+            choice = input.nextInt();
 
-        System.out.println("----------------------------------------------------------------------------------------------");
-        System.out.println("              Hello this program is for -GYM LOGIN SYSTEM-                                    ");
-        System.out.println("----------------------------------------------------------------------------------------------");
-        System.out.println("               please enter the number of choices that you want *-*                           \n");
-        System.out.println(" 1: Reserve an appointment ");
-        System.out.println(" 2: Scan the chip of the Member ");
-        System.out.println(" 3: Scan the chip of the Employee ");
-        System.out.println(" 4: Print report Management");
-        System.out.println(" 5: End program");
-        System.out.print("Pleas enter your choice Here:");
-        int choice = input.nextInt();
+            //--------------------------------
+            if (choice == 1) {
+                ReserveAppointment(input, MemberID);
+                System.out.println();
+                System.out.println();
 
-        //--------------------------------
-        if (choice == 1) {
-            ReserveAppointment(input, MemberID);
+            } else if (choice == 2) {
+                ScanChipOfMember();
 
-        } else if (choice == 2) {
-            ScanChipOfMember();
+            } else if (choice == 3) {
+                ScanChipOfEmployee();
 
-        } else if (choice == 3) {
-            ScanChipOfEmployee();
+            } else if (choice == 4) {
+                PrintReportManagement();
 
-        } else if (choice == 4) {
-            PrintReportManagement();
+            } else if (choice == 5) {
+                System.exit(0);
+            }
 
-        } else {
-            System.exit(0);
-        }
-
-        //End of the main 
+            //End of the main 
+        } while (choice != 5);
     }
 
     //----------------Methods----------------------
@@ -104,30 +109,15 @@ public class Gym_Login_System {
                 case 1:
 
                     //Check Time Avalibality
-                   full = CheckTimeAvalibalty(Appointment1,full);
+                    full = CheckTimeAvalibalty(Appointment1, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 1);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 1);
 
                     }
 
@@ -135,211 +125,104 @@ public class Gym_Login_System {
                 case 2:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment2,full);
+                    full = CheckTimeAvalibalty(Appointment2, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 2);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 2);
 
                     }
                     break;
                 case 3:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment3,full);
+                    full = CheckTimeAvalibalty(Appointment3, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 3);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 3);
 
                     }
                     break;
                 case 4:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment4,full);
+                    full = CheckTimeAvalibalty(Appointment4, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 4);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 4);
 
                     }
                     break;
                 case 5:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment5,full);
+                    full = CheckTimeAvalibalty(Appointment5, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 5);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 5);
 
                     }
                     break;
                 case 6:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment6,full);
+                    full = CheckTimeAvalibalty(Appointment6, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 6);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 6);
 
                     }
                     break;
                 case 7:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment7,full);
+                    full = CheckTimeAvalibalty(Appointment7, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
 
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 7);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 7);
 
                     }
                     break;
                 case 8:
 
                     //Check Time Avalibality
-                    full = CheckTimeAvalibalty(Appointment8,full);
+                    full = CheckTimeAvalibalty(Appointment8, full);
                     if (full) {
                         System.out.println("Sorry, this time is unavalibale! Please select another time");
 
                     } else {
                         System.out.println("Great, this time is avalibale");
-
-                        int enteredCode;
-                        do {
-                            System.out.print("Please Enter the verification code: ");
-                            enteredCode = input.nextInt();
-
-                            //Check Verification Code
-                            if (code[codeIndex] == enteredCode) {
-                                Appoinment appointment = new Appoinment(MemberID, 8);
-                                appointments.add(appointment);
-                                System.out.println("Appointment has been successfully booked. Have a nice day! ");
-                                codeIndex++;
-                                System.exit(0);
-                            } else {
-                                System.out.println("Incorrect code try again!");
-                            }
-
-                        } while (code[codeIndex] != enteredCode);
+                        //Verification Code
+                        CheckVerificationCode(input, code, codeIndex, MemberID, 8);
 
                     }
                     break;
@@ -355,25 +238,25 @@ public class Gym_Login_System {
         System.out.println();
         System.out.println(" -------------------------------------------------------------------- ");
         System.out.println("             Welcome, This is the Timetable for the gym:               ");
-        System.out.println(" ________________________");
+        System.out.println(" ______________________________________________________________________");
         System.out.println("                            Morning Time (AM)                          ");
-        System.out.println(" _________________________");
+        System.out.println(" _______________________________________________________________________");
         System.out.println(" Appointment Number      Branch        Day       Date        Time      ");
-        System.out.println("________________________");
+        System.out.println("________________________________________________________________________");
         System.out.println("          1:             AL-SAFA      Sunday   01-1-2023   07:00-09:00  ");
         System.out.println("          2:             AL-REHAB     Monday   02-1-2023   08:00-09:00  ");
         System.out.println("          3:             AL-BAWADI    Sunday   01-1-2023   07:00-09:00  ");
         System.out.println("          4:             AL-SAFA      Sunday   01-1-2023   09:00-11:00  ");
-        System.out.println("________________________");
+        System.out.println("________________________________________________________________________");
         System.out.println("                            Evening Time (PM)                          ");
-        System.out.println(" _________________________");
+        System.out.println(" _______________________________________________________________________");
         System.out.println(" Appointment Number      Branch        Day       Date        Time      ");
-        System.out.println("________________________");
+        System.out.println("________________________________________________________________________");
         System.out.println("          5:             AL-SAFA      Sunday   01-1-2023   12:00-02:00  ");
         System.out.println("          6:             AL-REHAB     Monday   02-1-2023   08:00-10:00  ");
         System.out.println("          7:             AL-BAWADI    Sunday   01-1-2023   07:00-09:00  ");
         System.out.println("          8:             AL-SAFA      Sunday   01-1-2023   10:00-11:00  ");
-        System.out.println("________________________");
+        System.out.println("________________________________________________________________________");
         System.out.println();
         System.out.print("  Enter The appointment Number: ");
     }
@@ -392,7 +275,6 @@ public class Gym_Login_System {
 
     }
 
-    
     public static boolean CheckTimeAvalibalty(Member x[], boolean full) {
 
         for (int i = 0; i < x.length; i++) {
@@ -401,5 +283,25 @@ public class Gym_Login_System {
             }
         }
         return full;
+    }
+
+    public static void CheckVerificationCode(Scanner input, int code[], int index, int MemberID, int appointmentTime) {
+        int enteredCode;
+        do {
+            System.out.print("Please Enter the verification code: ");
+            enteredCode = input.nextInt();
+
+            //Check Verification Code
+            if (code[codeIndex] == enteredCode) {
+                Appoinment appointment = new Appoinment(MemberID, appointmentTime);
+                appointments.add(appointment);
+                System.out.println("Appointment has been successfully booked. Have a nice day! ");
+                codeIndex++;
+                break;
+            } else {
+                System.out.println("Incorrect code try again!");
+            }
+
+        } while (code[codeIndex] != enteredCode);
     }
 }
